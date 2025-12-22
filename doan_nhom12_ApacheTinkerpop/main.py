d304c11dc128a1fdeea61f6e42ed6e0311573c99
EnditiNguyen <tiennguyen.31241021788@st.ueh.edu.vn>
2025-12-22 14:35:21 +0700

    Tiến có sửa để sử dụng data mẫu trong thư mục data

--- doan_nhom12_ApacheTinkerpop/main.py
+++ doan_nhom12_ApacheTinkerpop/main.py
@@ -1,13 +1,11 @@
-# main.py
 import sys
 import asyncio
 
-# Fix event loop cho Windows
 if sys.platform.startswith("win"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 from PyQt5.QtWidgets import QApplication
-from ui.main_window import MainWindow  # MainWindow đã tích hợp controller
+from ui.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
